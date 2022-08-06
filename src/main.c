ba045343a2512e2233b0e079985dcb5dd4823ab1
Lemuria <74448738+a-random-lemurian@users.noreply.github.com>
2022-08-07 04:20:37 +0800

    feat: add CLI interface

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 
 #include "utils.h"
+#include "argparse.h"
 
 #define LOADAVGMONITOR_DBFILE "/var/lib/loadavgmonitord/main.sqlite"
 
@@ -36,13 +37,13 @@ int check_db_file(char* file)
   }
 }
 
-int main(int argc, char **argv)
+void monitor_loadavg(char* file, int delay_ms)
 {
   for (;;) {
-    if (check_db_file(LOADAVGMONITOR_DBFILE) == 0) {
+    if (check_db_file(file) == 0) {
       sqlite3 *db;
       sqlite3_stmt *stmt;
-      sqlite3_open(LOADAVGMONITOR_DBFILE, &db);
+      sqlite3_open(file, &db);
       sqlite3_prepare(db,
                       "INSERT INTO loadavg (measured_at, "
                       "avg1m, avg5m, avg15m)"
@@ -60,8 +61,33 @@ int main(int argc, char **argv)
       sqlite3_close(db);
     }
     else {
-      prepare_new_db_file(LOADAVGMONITOR_DBFILE);
+      prepare_new_db_file(file);
     }
-    msleep(5000);
+
+    msleep(delay_ms);
   }
+}
+
+int main(int argc, char **argv)
+{
+  int delay_ms = 5000;
+  char* file = LOADAVGMONITOR_DBFILE;
+
+  struct argparse_option opts[] = {
+    OPT_HELP(),
+    OPT_GROUP("Basic options"),
+    OPT_STRING('f', "file", &file, "SQLite database file to write load average readings to, "LOADAVGMONITOR_DBFILE" by default"),
+    OPT_INTEGER('d', "delay", &delay_ms, "Delay between each measurement in milliseconds, 5 seconds by default"),
+    OPT_END()
+  };
+
+  struct argparse ap;
+  argparse_init(&ap, opts, NULL, 0);
+  argparse_describe(&ap, "Load average monitor",
+                    "-------------\n"
+                    "https://github.com/a-random-lemurian/loadavgmonitord\n"
+                    "Compiled "__DATE__" "__TIME__"");
+
+  argparse_parse(&ap, argc, (const char**)argv);
+  monitor_loadavg(file, delay_ms);
 }
